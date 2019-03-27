61617519ddcbef67b47fe559533f242fb6ace898
Raffaele Senften <rse105568@iet-gibb.ch>
2019-03-27 22:31:10 +0100

    an AB03 weitergearbeitet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,8 @@ class PagesController < ApplicationController
     if !user_signed_in?
       redirect_to new_user_session_path
     end
+    # if !user_signed_in?
+    #   hide-text
+    # end
   end
 end
