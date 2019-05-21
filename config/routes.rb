9ea967cca6901b209a79c093582ca473289bc676
Raffaele Senften <rse105568@iet-gibb.ch>
2019-05-21 22:36:25 +0200

    AB05 begonnen

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
+
+  resources :posts, only: [:index, :show, :create, :destroy] do
+    resources :photos, only: [:create]
+  end
 end
