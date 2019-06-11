c663100742810dedc7af5c51ed7c6556d484e684
Raffaele Senften <rse105568@iet-gibb.ch>
2019-06-11 16:25:00 +0200

    AB06 fertig

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#home'
   devise_for :users, controllers: { registrations: 'registrations' }
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
 
